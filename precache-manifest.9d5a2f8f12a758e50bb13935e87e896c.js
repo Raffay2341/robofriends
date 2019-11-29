e0d05c24d1241e8f17caf0defcf6cc9a13e297c1
Raffay Atiq <raffayatiq2341@gmail.com>
2019-11-29 23:57:22 +0500

    Updates

---
+++ precache-manifest.9d5a2f8f12a758e50bb13935e87e896c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec66838c452bfb91c84f31e02c39d5bd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "50a49d17343ff6fee5bd",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b21e33b45f72fd3eae0e",
+    "url": "/robofriends/static/css/main.e3ae1f75.chunk.css"
+  },
+  {
+    "revision": "50a49d17343ff6fee5bd",
+    "url": "/robofriends/static/js/2.cd16599c.chunk.js"
+  },
+  {
+    "revision": "b21e33b45f72fd3eae0e",
+    "url": "/robofriends/static/js/main.9d80be70.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
